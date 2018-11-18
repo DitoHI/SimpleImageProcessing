46b848ee4dc9248faa098283e6abf413f530e0c1
Kudo <ditohafizh@gmail.com>
2018-11-18 14:55:17 +0700

    This is the Test file to get you through the basic usages

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        BufferedImage image = null;
+        BufferedImage image;
 
         // get current directory
         Path currentRelativePath = Paths.get("");
